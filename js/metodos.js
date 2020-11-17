026f9479ec4f6babfd6e72d74b53b2f9b83238ed
David Velez <ddeivi_clip@yahoo.com>
2020-11-17 13:36:35 -0500

    funciones eliminar y editar

--- js/metodos.js
+++ js/metodos.js
@@ -7,6 +7,7 @@ function botonFormulario() {
 
     } else {
 
+        editarLibro();
     }
 
 }
@@ -70,6 +71,80 @@ function obtenerLibros() {
 
 
 
+function obtenerLibroEditar(idLibro) {
+
+    localStorage.setItem('idLibro', idLibro);
+
+    var list = ObtenerListaLibros();
+
+    document.getElementById("titulo").value = list[idLibro].titulo;
+    document.getElementById("autor").value = list[idLibro].autor;
+    document.getElementById("fecha").value = list[idLibro].fecha_publicacion;
+    document.getElementById("descripcion").value = list[idLibro].descripcion;
+
+    document.getElementById("descripcion").value = list[idLibro].descripcion;
+
+    btnCancelar = document.getElementById("btn-cancelar");
+    btnCancelar.style.display = "block";
+
+    btnAgg = document.getElementById("btn-agg").value = "Editar";
+
+    var containerForm = document.getElementById("container-form");
+    containerForm.scrollIntoView();
+
+
+}
+
+function editarLibro() {
+
+    var titulo = document.getElementById("titulo").value;
+    var autor = document.getElementById("autor").value;
+    var fecha = document.getElementById("fecha").value;
+    var descripcion = document.getElementById("descripcion").value;
+
+
+    if (titulo.length == 0 || autor.length == 0 || fecha.length == 0 || descripcion.length == 0) {
+        error();
+    } else {
+
+        var list = ObtenerListaLibros();
+
+        var idLibro = localStorage.getItem('idLibro');
+
+        var titulo = document.getElementById("titulo").value;
+        var autor = document.getElementById("autor").value;
+        var fecha = document.getElementById("fecha").value;
+        var descripcion = document.getElementById("descripcion").value;
+
+        list[idLibro].titulo = titulo;
+        list[idLibro].autor = autor;
+        list[idLibro].fecha_publicacion = fecha;
+        list[idLibro].descripcion = descripcion;
+
+        list.splice(idLibro, 1, list[idLibro]);
+
+        localStorage.setItem('ListaLibros', JSON.stringify(list));
+
+        obtenerLibros();
+        success();
+        cancelar();
+    }
+
+
+}
+
+function cancelar() {
+
+    document.getElementById("titulo").value = "";
+    document.getElementById("autor").value = "";
+    document.getElementById("fecha").value = "";
+    document.getElementById("descripcion").value = "";
+
+    btnCancelar = document.getElementById("btn-cancelar");
+    btnCancelar.style.display = "none";
+    btnAgg = document.getElementById("btn-agg").value = "Registrar libro";
+
+}
 
 
 
@@ -90,5 +165,41 @@ function error() {
         icon: 'error',
         showConfirmButton: false,
         timer: 1000
+    })
+}
+
+
+function borrarLibro(idLibro) {
+
+    var list = ObtenerListaLibros();
+    list.splice(idLibro, 1);
+    localStorage.setItem('ListaLibros', JSON.stringify(list));
+    obtenerLibros();
+
+}
+
+
+function eliminar(idLibro) {
+    Swal.fire({
+        title: '¿Eliminar este registro?',
+        text: "Este registro se eliminará",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar'
+
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            borrarLibro(idLibro);
+
+            Swal.fire(
+                'Correcto',
+                'Su registro ha sido eliminado',
+                'success'
+            )
+        }
     })
 }
